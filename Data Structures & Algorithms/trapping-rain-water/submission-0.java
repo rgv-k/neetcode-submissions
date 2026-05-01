1c5b8bab01ea382b1b565995154e1f4fda290579
Raghav <rgvndrak@gmail.com>
2026-05-01 19:57:26 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        suffix[n-1] = height[n-1];
+        prefix[0] = height[0];
+        for(int i =1; i < n; i++){
+            prefix[i] = Math.max(height[i], prefix[i-1]);
+            suffix[n-i-1] = Math.max(height[n-i-1],suffix[n-i]);
+        }
+        int total = 0;
+        for(int i = 1; i < n-1;i++){
+            int temp = (Math.min(prefix[i], suffix[i]) - height[i]);
+            if(temp>0) total += temp;
+        }
+
+        return total;
+    }
+}
